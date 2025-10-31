acc5c5978b9b47591fc590f2f550893d9bc32d9b
22-luis <0006720@uca.edu.sv>
2025-10-31 09:29:51 -0600

    Update: record_stopwatch.py & Delete: task_status_log.py

--- app/models/record_stopwatch.py
+++ app/models/record_stopwatch.py
@@ -1,18 +1,15 @@
 import uuid
-from sqlalchemy import Column, String, Integer, Float, DateTime, Boolean
+from sqlalchemy import Column, String, Integer, Float, DateTime, Boolean, ForeignKey
+from sqlalchemy.dialects.mysql import BIGINT
+from sqlalchemy.orm import relationship
+
 from app.db.database import Base
 from sqlalchemy.dialects.postgresql import UUID
 
 class RecordStopwatch(Base):
     __tablename__ = 'record_stopwatch'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    lote = Column(String, nullable=True)
-    code = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    people = Column(Integer, nullable=True)
+    task_id = Column(UUID(as_uuid=True), ForeignKey('task.id'), nullable=False)
     quantity = Column(Float, nullable=False)
-    real_quantity = Column(Float, nullable=False)
-    start_time = Column(DateTime, nullable=False)
-    end_time = Column(DateTime, nullable=True)
-    accumulated_duration = Column(Integer, default=0, nullable=False) # in seconds
-    is_paused = Column(Boolean, default=False, nullable=False)
+    accumulated_duration = Column(BIGINT, default=0, nullable=False) # in seconds
+    task = relationship('Task', back_populates='record_stopwatch')
