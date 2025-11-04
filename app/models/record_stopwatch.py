138310c7747a570cf37773cdf32189ac8672e1be
22-luis <00016720@uca.edu.sv>
2025-11-04 10:20:12 -0600

    Update: new date field in record_stopwatch

--- app/models/record_stopwatch.py
+++ app/models/record_stopwatch.py
@@ -1,12 +1,7 @@
 import uuid
-<<<<<<< HEAD
 from sqlalchemy import Column, Float, ForeignKey, BigInteger, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-=======
-from sqlalchemy import Column, Float, ForeignKey, BigInteger
-from sqlalchemy.orm import relationship
->>>>>>> 6e0846ff0532e9c61dd2cbf57cd1b22aab21b7be
 
 from app.db.database import Base
 from sqlalchemy.dialects.postgresql import UUID
@@ -17,8 +12,5 @@ class RecordStopwatch(Base):
     task_id = Column(UUID(as_uuid=True), ForeignKey('task.id'), nullable=False)
     quantity = Column(Float, nullable=False)
     accumulated_duration = Column(Float, default=0, nullable=False) # in hours
-<<<<<<< HEAD
     creation_date = Column(DateTime(timezone=True), server_default=func.now())
-=======
->>>>>>> 6e0846ff0532e9c61dd2cbf57cd1b22aab21b7be
     task = relationship('Task', back_populates='record_stopwatch')
--- app/schemas/record_stopwatch.py
+++ app/schemas/record_stopwatch.py
@@ -1,9 +1,6 @@
 from pydantic import BaseModel
 import uuid
-<<<<<<< HEAD
 from datetime import datetime
-=======
->>>>>>> 6e0846ff0532e9c61dd2cbf57cd1b22aab21b7be
 
 class RecordStopwatchBase(BaseModel):
     task_id: uuid.UUID
@@ -18,10 +15,7 @@ class RecordStopwatchUpdate(BaseModel):
 
 class RecordStopwatchInDBBase(RecordStopwatchBase):
     id: uuid.UUID
-<<<<<<< HEAD
     creation_date: datetime
-=======
->>>>>>> 6e0846ff0532e9c61dd2cbf57cd1b22aab21b7be
 
     class Config:
         orm_mode = True
