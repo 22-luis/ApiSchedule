103e8fd0cadaa6aa30ab45e5d0c7ecf1f9678aa9
luismolina-dev <luismolina.dev@gmail.com>
2025-12-18 14:12:11 -0600

    update: user endpoints

--- app/modules/core/api/routes_user.py
+++ app/modules/core/api/routes_user.py
@@ -122,7 +122,7 @@ def get_users(
 
 @router.get("/{user_id}", response_model=UserOut)
 def get_user(
-    user_id: UUID,
+    user_id: UserOut,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
@@ -133,7 +133,7 @@ def get_user(
 
 @router.get("/{user_id}/signature", response_model=UserOut)
 def get_user_signature(
-    user_id: UUID,
+    user_id: UserOut,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ):
