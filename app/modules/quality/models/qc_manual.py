82960fc154b3051c5bba20c15e6a55d07d765ac0
22-luis <00016720@uca.edu.sv>
2025-12-22 14:41:05 -0600

    feat: update qc_manual model to adjust createdAt timestamp and add migration scripts

--- app/modules/quality/models/qc_manual.py
+++ app/modules/quality/models/qc_manual.py
@@ -1,11 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 from sqlalchemy import Column, Integer, JSON, DateTime
 from app.shared.db.database import Base
 
 def get_time():
     tz = pytz.timezone('America/El_Salvador')
-    return datetime.now(tz)
+    return datetime.now(tz) - timedelta(hours=6)
 
 
 class QcManual(Base):
@@ -13,4 +13,4 @@ class QcManual(Base):
     id = Column(Integer, primary_key=True, index=True)
     version = Column(Integer, nullable=False)
     content = Column(JSON, nullable=False)
-    createdAt = Column(DateTime(timezone=True), default=get_time(), nullable=False)
+    createdAt = Column(DateTime(timezone=True), default=get_time, nullable=False)
