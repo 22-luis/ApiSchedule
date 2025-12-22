1700c540bb71d26fbe9150969f546a6e081b0c6b
luismolina-dev <luismolina.dev@gmail.com>
2025-12-22 14:04:20 -0600

    update: refine QC manual model to use timezone-specific createdAt timestamp

--- app/modules/quality/models/qc_manual.py
+++ app/modules/quality/models/qc_manual.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+import pytz
 from sqlalchemy import Column, Integer, JSON, DateTime
-from sqlalchemy.sql import func
 from app.shared.db.database import Base
+
+def get_time():
+    tz = pytz.timezone('America/El_Salvador')
+    return datetime.now(tz)
+
 
 class QcManual(Base):
     __tablename__ = "qc_manual"
     id = Column(Integer, primary_key=True, index=True)
     version = Column(Integer, nullable=False)
     content = Column(JSON, nullable=False)
-    createdAt = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    createdAt = Column(DateTime(timezone=True), default=get_time(), nullable=False)
